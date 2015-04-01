97de65ee9914ae4d865b5a17c8e720b78bd76aa7
ikhakoo <ikhakoo@gmail.com>
2015-04-01 15:37:11 -0400

    step 2

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,31 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-grocery_list.each do |item|
-	puts "* " + item
+puts "Welcome to Grocey List Manager!"
+puts "Here is a copy of your Grocery List:"
+
+def print_items(list)
+	list.each do |item|
+		puts "* " + item
+	end
+end
+
+print_items(grocery_list)
+
+puts "Do you want to (a)dd/(r)emove anything else to the list?"
+puts "If not, just type (d)one!"
+result = gets.chomp.downcase
+
+if result == "a" || result == "add"
+	puts "What would you like to add?"
+	add = gets.chomp
+	grocery_list << add
+	puts "Here is a copy of the updated list!"
+	print_items(grocery_list)
+elsif result == "r" || result == "remove"
+	puts "What would you like to remove?"
+	remove = gets.chomp
+	grocery_list.delete(remove)
+	puts "Here is an updated copy of the list!"
+	print_items(grocery_list)
+elsif result == "d" || result == "done"
+	puts "Thanks for using the Grocery List Manager"
 end
