d4cfd4cc8d8d3b56e8737d504fd8b1f13dfa72a1
ikhakoo <ikhakoo@gmail.com>
2015-04-01 16:16:29 -0400

    step 4 included option for bananas

--- exercise6.rb
+++ exercise6.rb
@@ -10,6 +10,13 @@ end
 
 print_items(grocery_list)
 puts "You have #{grocery_list.length} items in your list"
+
+if grocery_list.include?("bananas")
+	puts "You need to pick-up bananas"
+else
+	puts "You don't need to pick-up bananas"
+end
+
 puts "Do you want to (a)dd/(r)emove anything else to the list?"
 puts "If not, just type (d)one!"
 result = gets.chomp.downcase
