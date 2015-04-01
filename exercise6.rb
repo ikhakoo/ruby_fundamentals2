7ba22888ad459d5d455e3c4819ac349447a81227
ikhakoo <ikhakoo@gmail.com>
2015-04-01 16:06:31 -0400

    step 3 - displays count now

--- exercise6.rb
+++ exercise6.rb
@@ -9,7 +9,7 @@ def print_items(list)
 end
 
 print_items(grocery_list)
-
+puts "You have #{grocery_list.length} items in your list"
 puts "Do you want to (a)dd/(r)emove anything else to the list?"
 puts "If not, just type (d)one!"
 result = gets.chomp.downcase
@@ -20,12 +20,14 @@ if result == "a" || result == "add"
 	grocery_list << add
 	puts "Here is a copy of the updated list!"
 	print_items(grocery_list)
+	puts "You have #{grocery_list.length} items in your list"
 elsif result == "r" || result == "remove"
 	puts "What would you like to remove?"
 	remove = gets.chomp
 	grocery_list.delete(remove)
 	puts "Here is an updated copy of the list!"
 	print_items(grocery_list)
+	puts "You have #{grocery_list.length} items in your list"
 elsif result == "d" || result == "done"
 	puts "Thanks for using the Grocery List Manager"
 end
